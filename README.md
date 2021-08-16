a7e02e87219537c18ff8eb65224e5f6872ab00f4
Fiona Apps <wikipanyd@gmail.com>
2021-08-16 14:33:44 +0100

    created environment and set up test suite

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+----------
+BOOKMARK PROJECT
+----------
+
 User stories:
 
 1. 
---
+++ spec/feature_tests/index_spec.rb
@@ -0,0 +1,6 @@
+feature 'displays index page' do
+    scenario "displays 'Hello World'" do
+        visit('/')
+        expect(page).to have_content 'Hello World!'
+    end
+end
